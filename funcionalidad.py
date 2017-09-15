03c63abee1a64ec4ab461afe895042b6eeb5f21c
Juan Alvarado <CltControl@GGIECSOFWRK160.espol.edu.ec>
2017-09-15 09:56:26 -0500

    test

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+def AprobarPrestamo(años_de_trabajo, estado_civil, num_dependientes, capacidad_endeudamiento, años_vivienda):
+    res = "D"
+    if(años_de_trabajo >= 2):
+        if(estado_civil=="divorciado"):
+            if(num_dependientes>0):
+                res = "A"
+            elif(num_dependientes==0):
+                res = "M"
+        elif(estado_civil=="soltero"):
+            res="D"
+        elif(estado_civil=="casado"):
+            res="A"
+    elif(años_de_trabajo<2):
+        if(capacidad_endeudamiento>=0.75):
+            res = "M"
+        elif(capacidad_endeudamiento<0.75):
+            if(años_vivienda<1.5):
+                res="D"
+            else:
+                res="A"
+    return res
--- test.py
+++ test.py
@@ -10,8 +10,78 @@ class Test(unittest.TestCase):
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
     
-    def test_prueba_ID(self):
-        self.assertEquals()
+    def test_prueba_1(self):
+        años_de_trabajo = 3
+        estado_civil = "divorciado"
+        num_dependientes = 1
+        capacidad_endeudamiento = 0.75
+        años_vivienda = 2
+        res = funcionalidad.AprobarPrestamo(años_de_trabajo, estado_civil, num_dependientes, capacidad_endeudamiento, años_vivienda)
+        self.assertEquals("A", res)
+
+    def test_prueba_2(self):
+        años_de_trabajo = 3
+        estado_civil = "divorciado"
+        num_dependientes = 0
+        capacidad_endeudamiento = 0.75
+        años_vivienda = 2
+        res = funcionalidad.AprobarPrestamo(años_de_trabajo, estado_civil, num_dependientes, capacidad_endeudamiento, años_vivienda)
+        self.assertEquals("M", res)
+
+    def test_prueba_3(self):
+        años_de_trabajo = 3
+        estado_civil = "soltero"
+        num_dependientes = 1
+        capacidad_endeudamiento = 0.75
+        años_vivienda = 2
+        res = funcionalidad.AprobarPrestamo(años_de_trabajo, estado_civil, num_dependientes, capacidad_endeudamiento, años_vivienda)
+        self.assertEquals("D", res)
+
+    def test_prueba_4(self):
+        años_de_trabajo = 3
+        estado_civil = "casado"
+        num_dependientes = 1
+        capacidad_endeudamiento = 0.75
+        años_vivienda = 2
+        res = funcionalidad.AprobarPrestamo(años_de_trabajo, estado_civil, num_dependientes, capacidad_endeudamiento, años_vivienda)
+        self.assertEquals("A", res)
+
+    def test_prueba_5(self):
+        años_de_trabajo = 1
+        estado_civil = "divorciado"
+        num_dependientes = 1
+        capacidad_endeudamiento = 0.8
+        años_vivienda = 1
+        res = funcionalidad.AprobarPrestamo(años_de_trabajo, estado_civil, num_dependientes, capacidad_endeudamiento, años_vivienda)
+        self.assertEquals("M", res)
+
+    def test_prueba_6(self):
+        años_de_trabajo = 1
+        estado_civil = "divorciado"
+        num_dependientes = 1
+        capacidad_endeudamiento = 0.55
+        años_vivienda = 1
+        res = funcionalidad.AprobarPrestamo(años_de_trabajo, estado_civil, num_dependientes, capacidad_endeudamiento, años_vivienda)
+        self.assertEquals("D", res)
+
+    def test_prueba_7(self):
+        años_de_trabajo = 1
+        estado_civil = "divorciado"
+        num_dependientes = 1
+        capacidad_endeudamiento = 0.55
+        años_vivienda = 2
+        res = funcionalidad.AprobarPrestamo(años_de_trabajo, estado_civil, num_dependientes, capacidad_endeudamiento, años_vivienda)
+        self.assertEquals("A", res)
+
+    def test_prueba_8(self):
+        años_de_trabajo = 3
+        estado_civil = "otros"
+        num_dependientes = 1
+        capacidad_endeudamiento = 0.55
+        años_vivienda = 1
+        res = funcionalidad.AprobarPrestamo(años_de_trabajo, estado_civil, num_dependientes, capacidad_endeudamiento, años_vivienda)
+        self.assertEquals("D", res)
+
             
 if __name__ == '__main__':
     unittest.main()
