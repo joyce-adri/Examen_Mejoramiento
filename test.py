0d0f684dab0bcde7e3b44190206ae3a4e71b974b
mvelasco93 <m_velasco93@hotmail.com>
2017-09-15 09:36:48 -0500

    Test 1

--- test.py
+++ test.py
@@ -9,8 +9,13 @@ class Test(unittest.TestCase):
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
     
-    def test_prueba_ID(self):
-        self.assertEquals()
+    def test_prueba_1(self):
+
+    #dependientes, estado, capacidaddeuda, tiempovivienda, tiempotrabajo, edad
+        result_val = funcionalidad.aprobarPrestamo(1, "soltero", 20, 3, 1, 17)
+        expected_val = "D"
+
+        self.assertEquals(result_val, expected_val)
             
 if __name__ == '__main__':
     unittest.main()
