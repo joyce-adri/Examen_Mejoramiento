0b0c95d26de8375a99e9d06e6fd98c518b8cf9ca
Washington Vélez <wjvelez@espol.edu.ec>
2017-09-15 09:23:05 -0500

    creada funcion, test1

--- test.py
+++ test.py
@@ -6,12 +6,13 @@ import funcionalidad
 
 
 class Test(unittest.TestCase):
-    
+
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
-    
-    def test_prueba_ID(self):
-        self.assertEquals()
-            
+
+    def test_prueba_1(self):
+        prestamo = funcionalidad.aprobar_prestamo(1, 0.40, 1, 'soltero', 1)
+        self.assertEquals(prestamo, 'D')
+
 if __name__ == '__main__':
     unittest.main()
