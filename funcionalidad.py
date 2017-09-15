c76ec235e73aef3dc4373e1cbf1540296c8b744e
mvelasco93 <m_velasco93@hotmail.com>
2017-09-15 09:36:40 -0500

    Funcionalidad

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+def aprobarPrestamo(dependientes, estado, capacidaddeuda, tiempovivienda, tiempotrabajo, edad):
+    if(edad>=18):
+        if(tiempotrabajo>=2):
+            if(estado == "divorciado"):
+                if(dependientes ==0):
+                    return "M"
+                elif(dependientes >0):
+                    return "A"
+            elif(estado == "soltero"):
+                return "D"
+            elif(estado == "casado"):
+                return "A"
+        else:
+            if(capacidaddeuda >= 75):
+                return "M"
+            else:
+                if(tiempovivienda >=1.5):
+                    return "A"
+                else:
+                    return "D"
+    else:
+        return "D"
