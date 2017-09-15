85a9afb76d0a14763eb41d4f28cef3361c74972d
Washington Vélez <wjvelez@espol.edu.ec>
2017-09-15 10:22:17 -0500

    ..final

--- test.py
+++ test.py
@@ -15,32 +15,32 @@ class Test(unittest.TestCase):
         cap_end = 0.40
         años_viv = 1
         prestamo = funcionalidad.aprobar_prestamo(años_trab, cap_end, años_viv, 'soltero', 1)
-        self.assertEquals(prestamo, 'D')
+        self.assertEqual(prestamo, 'D')
 
     def test_prueba_2(self):
         años_trab = 1
         cap_end = 0.40
         años_viv = 3
         prestamo = funcionalidad.aprobar_prestamo(años_trab, cap_end, años_viv, 'soltero', 1)
-        self.assertEquals(prestamo, 'A')
+        self.assertEqual(prestamo, 'A')
 
     def test_prueba_3(self):
         años_trab = 1
         cap_end = 0.80
         prestamo = funcionalidad.aprobar_prestamo(años_trab, cap_end, 1, 'soltero', 1)
-        self.assertEquals(prestamo, 'M')
+        self.assertEqual(prestamo, 'M')
 
     def test_prueba_4(self):
         años_trab = 3
         estado_civil = 'casado'
         prestamo = funcionalidad.aprobar_prestamo(años_trab, 0.5, 1, estado_civil, 1)
-        self.assertEquals(prestamo, 'A')
+        self.assertEqual(prestamo, 'A')
 
     def test_prueba_5(self):
         años_trab = 3
         estado_civil = 'soltero'
         prestamo = funcionalidad.aprobar_prestamo(años_trab, 0.5, 1, estado_civil, 1)
-        self.assertEquals(prestamo, 'D')
+        self.assertEqual(prestamo, 'D')
 
 
     def test_prueba_6(self):
@@ -48,7 +48,7 @@ class Test(unittest.TestCase):
         estado_civil = 'divorciado'
         dep = 0
         prestamo = funcionalidad.aprobar_prestamo(años_trab, 0.5, 1, estado_civil, dep)
-        self.assertEquals(prestamo, 'M')
+        self.assertEqual(prestamo, 'M')
 
 
     def test_prueba_7(self):
@@ -56,7 +56,7 @@ class Test(unittest.TestCase):
         estado_civil = 'divorciado'
         dep = 2
         prestamo = funcionalidad.aprobar_prestamo(años_trab, 0.5, 1, estado_civil, dep)
-        self.assertEquals(prestamo, 'A')
+        self.assertEqual(prestamo, 'A')
 
 
 if __name__ == '__main__':
