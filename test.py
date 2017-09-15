b78d7f01e756a9c2ffcf6dd0486bf9f81d87d304
Juan Alvarado <CltControl@GGIECSOFWRK157.espol.edu.ec>
2017-09-15 09:44:16 -0500

    case1

--- test.py
+++ test.py
@@ -14,37 +14,6 @@ class Test(unittest.TestCase):
 		p = funcionalidad.AprobarPrestamo(1, "Divorced", 0.75, 1, 2)
 		self.assertEquals(p, "A")
 
-	def test_prueba_2(self):
-		p = funcionalidad.AprobarPrestamo(0, "Divorced", 0.75, 1, 2)
-		self.assertEquals(p, "M")
-
-	def test_prueba_3(self):
-		p = funcionalidad.AprobarPrestamo(1, "Single", 0.75, 1, 2)
-		self.assertEquals(p, "D")
-
-	def test_prueba_4(self):
-		p = funcionalidad.AprobarPrestamo(1, "Married", 0.75, 1, 2)
-		self.assertEquals(p, "A")
-
-	def test_prueba_5(self):
-		p = funcionalidad.AprobarPrestamo(1, "Divorced", 0.5, 1, 1)
-		self.assertEquals(p, "D")
-
-	def test_prueba_6(self):
-		p = funcionalidad.AprobarPrestamo(1, "Divorced", 0.5, 1.5, 1)
-		self.assertEquals(p, "A")
-
-	def test_prueba_7(self):
-		p = funcionalidad.AprobarPrestamo(1, "Married", 0.75, 1, 1)
-		self.assertEquals(p, "M")
-
-	def test_prueba_8(self):
-		p = funcionalidad.AprobarPrestamo(-1, "Divorced", 0.75, 1, 2)
-		self.assertEquals(p, "D")
-
-	def test_prueba_9(self):
-		p = funcionalidad.AprobarPrestamo(1, "dasdasd", 0.75, 1, 2)
-		self.assertEquals(p, "D")
             
 if __name__ == '__main__':
     unittest.main()
