4ee27e3182f9dcb7f6de9bf4bd9ff98c0c85e1de
lkuffo <lkuffo@espol.edu.ec>
2017-09-15 09:24:53 -0500

    Pruebas

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+def aprobar_prestamo(yearsTrabajo, end, estCivil, dep, yearsVivienda):
+    if yearsTrabajo >= 2:
+        if estCivil == "Single":
+            return "D"
+        elif estCivil == "Married":
+            return "A"
+        elif estCivil == "Divorced":
+            if dep > 0:
+                return "A"
+            elif dep == 0:
+                return "M"
+    elif yearsTrabajo < 2:
+        if end >= 75:
+            return "M"
+        elif end < 75:
+            if yearsVivienda < 1.5:
+                return "D"
+            elif yearsVivienda >= 1.5:
+                return "A"
--- test.py
+++ test.py
@@ -10,8 +10,83 @@ class Test(unittest.TestCase):
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
     
-    def test_prueba_ID(self):
-        self.assertEquals()
+    def test_prueba_1(self):
+        yearsTrabajo = 5
+        estCivil = "Single"
+        dep = 0
+        end = 75
+        yearsVivienda = 4
+
+        resultado = funcionalidad.aprobar_prestamo(yearsTrabajo, end, estCivil, dep, yearsVivienda)
+        esperado = "D"
+        self.assertEquals(resultado, esperado)
+
+    def test_prueba_2(self):
+        yearsTrabajo = 5
+        estCivil = "Married"
+        dep = 0
+        end = 60
+        yearsVivienda = 6
+
+        resultado = funcionalidad.aprobar_prestamo(yearsTrabajo, end, estCivil, dep, yearsVivienda)
+        esperado = "A"
+        self.assertEquals(resultado, esperado)
+
+    def test_prueba_3(self):
+        yearsTrabajo = 6
+        estCivil = "Divorced"
+        dep = 0
+        end = 10
+        yearsVivienda = 10
+
+        resultado = funcionalidad.aprobar_prestamo(yearsTrabajo, end, estCivil, dep, yearsVivienda)
+        esperado = "M"
+        self.assertEquals(resultado, esperado)
+
+    def test_prueba_4(self):
+        yearsTrabajo = 6
+        estCivil = "Divorced"
+        dep = 3
+        end = 15
+        yearsVivienda = 5
+
+        resultado = funcionalidad.aprobar_prestamo(yearsTrabajo, end, estCivil, dep, yearsVivienda)
+        esperado = "A"
+        self.assertEquals(resultado, esperado)
+
+    def test_prueba_5(self):
+        yearsTrabajo = 1
+        estCivil = "Married"
+        dep = 1
+        end = 75
+        yearsVivienda = 1
+
+        resultado = funcionalidad.aprobar_prestamo(yearsTrabajo, end, estCivil, dep, yearsVivienda)
+        esperado = "M"
+        self.assertEquals(resultado, esperado)
+
+    def test_prueba_6(self):
+        yearsTrabajo = 1
+        estCivil = "Single"
+        dep = 2
+        end = 60
+        yearsVivienda = 3
+
+        resultado = funcionalidad.aprobar_prestamo(yearsTrabajo, end, estCivil, dep, yearsVivienda)
+        esperado = "A"
+        self.assertEquals(resultado, esperado)
+
+    def test_prueba_7(self):
+        yearsTrabajo = 1
+        estCivil = "Divorced"
+        dep = 0
+        end = 50
+        yearsVivienda = 1
+
+        resultado = funcionalidad.aprobar_prestamo(yearsTrabajo, end, estCivil, dep, yearsVivienda)
+        esperado = "D"
+        self.assertEquals(resultado, esperado)
+
             
 if __name__ == '__main__':
     unittest.main()
