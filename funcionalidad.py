5821a9cc6f4e16526561f6bf6c9a0a419db5fd66
chaljara <chaljara@espol.edu.ec>
2017-09-15 10:05:33 -0500

    Update funcionalidad.py

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+class Banco():
+    def aprobarPrestamo(self, endeudamiento, estadocivil, aniosvivienda, aniostrabajo, dependientes):
+        if aniostrabajo < 2:
+
+            if endeudamiento < 75:
+
+                if aniosvivienda < 1.5:
+                    return 'D'
+                else:
+                    return 'A'
+
+            else:
+                return 'M'
+        else:
+
+            if estadocivil == 'CASADO':
+                return 'A'
+            elif estadocivil == 'SOLTERO':
+                return 'D'
+            elif estadocivil == 'DIVORCIADO':
+
+                if dependientes == 0:
+                    return 'M'
+                else:
+                    return 'A'
+            else:
+                return 'D'
