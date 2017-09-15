b99ce3272331702c96c71ecd04bd5aa63f54611e
josealcivar <CltControl@GGIECSOFWRK131.espol.edu.ec>
2017-09-15 10:06:14 -0500

    Luis lainez pinto
    Hechas las funcionalidaddes y test, falto la prueba con coveralls

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+class Prestamo:
+    num_dependientes=0
+    estado_civil=""
+    capacidad_endeudamiento=0
+    ano_viviendaactual=0
+    ano_trabajoactual=0
+    edad=0
+    def __init__(self, num_dependientes, estado_civil, capacidad_endeudamiento, ano_viviendaactual, ano_trabajoactual, edad):
+        self.num_dependientes = num_dependientes
+        self.estado_civil = estado_civil
+        self.capacidad_endeudamiento = capacidad_endeudamiento
+        self.ano_viviendaactual = ano_viviendaactual
+        self.ano_trabajoactual = ano_trabajoactual
+        self.edad = edad
+
+class Programa:
+    def AprobarPrestamo(self, prestamo):
+        if(prestamo.ano_trabajoactual>=2):
+            if(prestamo.estado_civil=="Married"):
+                print "A"
+            if(prestamo.estado_civil=="Single"):
+                print "D"
+            if(prestamo.estado_civil=="Divorced"):
+                if(prestamo.num_dependientes>0):
+                    print "A"
+                else:
+                    print "M"
+
+        else:
+            if(prestamo.capacidad_endeudamiento>=75):
+                print "M"
+            else:
+                if(prestamo.ano_viviendaactual>=1.5):
+                    print "A"
+                else:
+                    print "D"
+
--- test.py
+++ test.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 
 import unittest
 import funcionalidad
+from funcionalidad import *
+
+#from Prestamo import prestamo
+#from Programa import programa
 
 
 class Test(unittest.TestCase):
@@ -10,8 +14,48 @@ class Test(unittest.TestCase):
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
     
-    def test_prueba_ID(self):
-        self.assertEquals()
-            
+    def test_1(self):
+        prestamo= Prestamo(0,"Married",0,0,2,0)
+        programa = Programa()
+        msg = programa.AprobarPrestamo(prestamo1)
+        self.assertEquals(msg, "A")
+
+    def test_2(self):
+        prestamo= Prestamo(0,"Single",0,0,2,0)
+        programa = Programa()
+        msg = programa.AprobarPrestamo(prestamo1)
+        self.assertEquals(msg, "D")
+
+    def test_3(self):
+        prestamo= Prestamo(1,"Divorced",0,0,2,0)
+        programa = Programa()
+        msg = programa.AprobarPrestamo(prestamo1)
+        self.assertEquals(msg, "A")
+
+    def test_4(self):
+        prestamo= Prestamo(0,"Divorced",0,0,2,0)
+        programa = Programa()
+        msg = programa.AprobarPrestamo(prestamo1)
+        self.assertEquals(msg, "M")
+
+    def test_5(self):
+        prestamo= Prestamo(0,"",75,0,1,0)
+        programa = Programa()
+        msg = programa.AprobarPrestamo(prestamo1)
+        self.assertEquals(msg, "M")
+
+    def test_6(self):
+        prestamo= Prestamo(0,"",74,1,1,0)
+        programa = Programa()
+        msg = programa.AprobarPrestamo(prestamo1)
+        self.assertEquals(msg, "D")
+
+    def test_7(self):
+        prestamo= Prestamo(0,"",74,2,1,0)
+        programa = Programa()
+        msg = programa.AprobarPrestamo(prestamo1)
+        self.assertEquals(msg, "A")
+
+
 if __name__ == '__main__':
     unittest.main()
