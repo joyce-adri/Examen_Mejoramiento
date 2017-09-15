487fa597f44709b954d0a0d0fe69e34c2cfb1958
chaljara <chaljara@espol.edu.ec>
2017-09-15 10:05:11 -0500

    Update test.py

--- test.py
+++ test.py
@@ -9,9 +9,39 @@ class Test(unittest.TestCase):
     
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
-    
-    def test_prueba_ID(self):
-        self.assertEquals()
+    #def aprobarPrestamo(self, endeudamiento, estadocivil, aniosvivienda, aniostrabajo, dependientes):
+
+    def test_prueba_1(self):
+        tipo = funcionalidad.Banco().aprobarPrestamo(50, 'SOLTERO', 1.0, 1, 0)
+        self.assertEqual(tipo, 'D')
+
+    def test_prueba_2(self):
+        tipo = funcionalidad.Banco().aprobarPrestamo(50, 'SOLTERO', 2.0, 1, 0)
+        self.assertEqual(tipo, 'A')
+
+    def test_prueba_3(self):
+        tipo = funcionalidad.Banco().aprobarPrestamo(80, 'SOLTERO', 2.0, 1, 0)
+        self.assertEqual(tipo, 'M')
+
+    def test_prueba_4(self):
+        tipo = funcionalidad.Banco().aprobarPrestamo(50, 'CASADO', 1.0, 5, 0)
+        self.assertEqual(tipo, 'A')
+
+    def test_prueba_5(self):
+        tipo = funcionalidad.Banco().aprobarPrestamo(50, 'SOLTERO', 1.0, 5, 0)
+        self.assertEqual(tipo, 'D')
+
+    def test_prueba_6(self):
+        tipo = funcionalidad.Banco().aprobarPrestamo(50, 'DIVORCIADO', 1.0, 5, 2)
+        self.assertEqual(tipo, 'A')
+
+    def test_prueba_7(self):
+        tipo = funcionalidad.Banco().aprobarPrestamo(50, 'DIVORCIADO', 1.0, 5, 0)
+        self.assertEqual(tipo, 'M')
+
+    def test_prueba_8(self):
+        tipo = funcionalidad.Banco().aprobarPrestamo(50, 'VIUDO', 1.0, 5, 0)
+        self.assertEqual(tipo, 'D')
             
 if __name__ == '__main__':
     unittest.main()
