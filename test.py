55f7cc0810a3432f615a7c6a779179ffe8bb6944
Washington Vélez <wjvelez@espol.edu.ec>
2017-09-15 10:20:32 -0500

    ...

--- test.py
+++ test.py
@@ -10,7 +10,6 @@ class Test(unittest.TestCase):
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
 
-
     def test_prueba_1(self):
         años_trab = 1
         cap_end = 0.40
@@ -25,20 +24,17 @@ class Test(unittest.TestCase):
         prestamo = funcionalidad.aprobar_prestamo(años_trab, cap_end, años_viv, 'soltero', 1)
         self.assertEquals(prestamo, 'A')
 
-
     def test_prueba_3(self):
         años_trab = 1
         cap_end = 0.80
         prestamo = funcionalidad.aprobar_prestamo(años_trab, cap_end, 1, 'soltero', 1)
         self.assertEquals(prestamo, 'M')
 
-
     def test_prueba_4(self):
         años_trab = 3
         estado_civil = 'casado'
         prestamo = funcionalidad.aprobar_prestamo(años_trab, 0.5, 1, estado_civil, 1)
         self.assertEquals(prestamo, 'A')
-
 
     def test_prueba_5(self):
         años_trab = 3
