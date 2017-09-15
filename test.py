75a2230659782b7f1cd328987d6895676531519b
Washington Vélez <wjvelez@espol.edu.ec>
2017-09-15 09:54:44 -0500

    test3

--- test.py
+++ test.py
@@ -24,22 +24,23 @@ class Test(unittest.TestCase):
         cap_end = 0.40
         años_viv = 3
         prestamo = funcionalidad.aprobar_prestamo(años_trab, cap_end, años_viv, 'soltero', 1)
-        print ('funcion--> ', prestamo)
         self.assertEquals(prestamo, 'A')
 
-    '''
+
     def test_prueba_3(self):
         años_trab = 1
         cap_end = 0.80
         prestamo = funcionalidad.aprobar_prestamo(años_trab, cap_end, 1, 'soltero', 1)
         self.assertEquals(prestamo, 'M')
 
+    '''
     def test_prueba_4(self):
         años_trab = 3
         estado_civil = 'casado'
         prestamo = funcionalidad.aprobar_prestamo(años_trab, 0.5, 1, estado_civil, 1)
         self.assertEquals(prestamo, 'A')
 
+    
     def test_prueba_5(self):
         años_trab = 3
         estado_civil = 'soltero'
