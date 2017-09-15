891609f45eb82029c8340a189a340855fdaf40ea
gaalocastillo <gadacast@espol.edu.ec>
2017-09-15 09:39:47 -0500

    tests

--- test.py
+++ test.py
@@ -4,8 +4,26 @@ import unittest
 from funcionalidad import *
 class Test(unittest.TestCase):
 	def test_prueba_1(self):
+		msg=AprobarPrestamo(0,"Married",0,0,4)
+		self.assertEquals(msg,"Aprobado")
+	def test_prueba_2(self):
 		msg=AprobarPrestamo(0,"Single",0,0,4)
 		self.assertEquals(msg,"Denegado")
+	def test_prueba_3(self):
+		msg=AprobarPrestamo(0,"Divorced",0,0,4)
+		self.assertEquals(msg,"Manual")
+	def test_prueba_4(self):
+		msg=AprobarPrestamo(5,"Divorced",0,0,4)
+		self.assertEquals(msg,"Aprobado")
+	def test_prueba_5(self):
+		msg=AprobarPrestamo(0,"Divorced",80,0,1)
+		self.assertEquals(msg,"Manual")
+	def test_prueba_6(self):
+		msg=AprobarPrestamo(0,"Divorced",60,1,1)
+		self.assertEquals(msg,"Denegado")
+	def test_prueba_7(self):
+		msg=AprobarPrestamo(0,"Divorced",60,2,1)
+		self.assertEquals(msg,"Aprobado")
 	
 if __name__ == '__main__':
     unittest.main()
