6b596885f3896c768bdc4731b7a1e93ae727a378
AnaBelen93 <anita-belen93@hotmail.com>
2017-09-15 10:31:12 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,42 +6,38 @@ import funcionalidad
 
 
 class Test(unittest.TestCase):
-   def test1(self):
-        estado = funcionalidad.Aprobar_Prestamo(1,70,1.0,1,3)
-        esperado = "Estado del prestamo:D"
-        self.assertEquals(estado, esperado)
-           
-    def test2(self):
+          
+    def test_1(self):
         estado = funcionalidad.Aprobar_Prestamo(1,70,1.0,1,3)
         esperado = "Estado del prestamo:D"
         self.assertEquals(estado, esperado)
 
-    def test3(self):
+    def test_2(self):
         estado = funcionalidad.Aprobar_Prestamo(1,70,1.6,1,3)
         esperado = "Estado del prestamo:A"
         self.assertEquals(estado, esperado)
         
-    def test3(self):
+    def test_3(self):
         estado = funcionalidad.Aprobar_Prestamo(1,76,1.0,1,3)
         esperado = "Estado del prestamo:M"
         self.assertEquals(estado, esperado)
 
-    def test4(self):
+    def test_4(self):
         estado = funcionalidad.Aprobar_Prestamo(3,70,1.0,1,3)
         esperado = "Estado del prestamo:A"
         self.assertEquals(estado, esperado)
 
-    def test5(self):
+    def test_5(self):
         estado = funcionalidad.Aprobar_Prestamo(3,70,1.0,1,0)
         esperado = "Estado del prestamo:M"
         self.assertEquals(estado, esperado)
 
-    def test6(self):
+    def test_6(self):
         estado = funcionalidad.Aprobar_Prestamo(3,70,1.0,2,3)
         esperado = "Estado del prestamo:D"
         self.assertEquals(estado, esperado)
 
-    def test7(self):
+    def test_7(self):
         estado = funcionalidad.Aprobar_Prestamo(3,70,1.0,3,3)
         esperado = "Estado del prestamo:A"
         self.assertEquals(estado, esperado)
