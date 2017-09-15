e0c63bb9834f401364c8450b5cc5b0934c5ec2f0
Ana Belen Yagual Meza <anita-belen93@hotmail.com>
2017-09-15 10:49:26 -0500

    test completos

--- test.py
+++ test.py
@@ -6,12 +6,42 @@ import funcionalidad
 
 
 class Test(unittest.TestCase):
-    
-# >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
-# Reemplazar EL ID por el numero correspondiente del caso de prueba
-    
-    def test_prueba_ID(self):
-        self.assertEquals()
+
+    def test_1(self):
+        estado = funcionalidad.Aprobar_Prestamo(1, 70, 1.0, 1, 3)
+        esperado = "Estado del prestamo: D"
+        self.assertEquals(estado, esperado)
+
+    def test_2(self):
+        estado = funcionalidad.Aprobar_Prestamo(1, 70, 1.6, 1, 3)
+        esperado = "Estado del prestamo: A"
+        self.assertEquals(estado, esperado)
+
+    def test_3(self):
+        estado = funcionalidad.Aprobar_Prestamo(1, 76, 1.0, 1, 3)
+        esperado = "Estado del prestamo: M"
+        self.assertEquals(estado, esperado)
+
+    def test_4(self):
+        estado = funcionalidad.Aprobar_Prestamo(3, 70, 1.0, 1, 3)
+        esperado = "Estado del prestamo: A"
+        self.assertEquals(estado, esperado)
+
+    def test_5(self):
+        estado = funcionalidad.Aprobar_Prestamo(3, 70, 1.0, 1, 0)
+        esperado = "Estado del prestamo: M"
+        self.assertEquals(estado, esperado)
+
+    def test_6(self):
+        estado = funcionalidad.Aprobar_Prestamo(3, 70, 1.0, 2, 3)
+        esperado = "Estado del prestamo: D"
+        self.assertEquals(estado, esperado)
+
+    def test_7(self):
+        estado = funcionalidad.Aprobar_Prestamo(3, 70, 1.0, 3, 3)
+        esperado = "Estado del prestamo: A"
+        self.assertEquals(estado, esperado)
+
             
 if __name__ == '__main__':
     unittest.main()
