c4b566ea655a1e50f48f2efe758fffaf02de6738
AnaBelen93 <anita-belen93@hotmail.com>
2017-09-15 10:24:02 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,54 +6,53 @@ import funcionalidad
 
 
 class Test(unittest.TestCase):
-def test1(self):
+   def test1(self):
 
         estado = funcionalidad.Aprobar_Prestamo(1,70,1.0,1,3)
-        esperado = "Estado del pretsamo:D"
+        esperado = "Estado del prestamo:D"
         self.assertEquals(estado, esperado)
            
     def test2(self):
 
         estado = funcionalidad.Aprobar_Prestamo(1,70,1.0,1,3)
-        esperado = "Estado del pretsamo:A"
+        esperado = "Estado del prestamo:D"
         self.assertEquals(estado, esperado)
 
     def test3(self):
 
         estado = funcionalidad.Aprobar_Prestamo(1,70,1.6,1,3)
-        esperado = "Estado del pretsamo:A"
+        esperado = "Estado del prestamo:A"
         self.assertEquals(estado, esperado)
         
     def test3(self):
 
         estado = funcionalidad.Aprobar_Prestamo(1,76,1.0,1,3)
-        esperado = "Estado del pretsamo:M"
+        esperado = "Estado del prestamo:M"
         self.assertEquals(estado, esperado)
 
     def test4(self):
 
         estado = funcionalidad.Aprobar_Prestamo(3,70,1.0,1,3)
-        esperado = "Estado del pretsamo:A"
+        esperado = "Estado del prestamo:A"
         self.assertEquals(estado, esperado)
 
     def test5(self):
 
         estado = funcionalidad.Aprobar_Prestamo(3,70,1.0,1,0)
-        esperado = "Estado del pretsamo:M"
+        esperado = "Estado del prestamo:M"
         self.assertEquals(estado, esperado)
 
     def test6(self):
 
         estado = funcionalidad.Aprobar_Prestamo(3,70,1.0,2,3)
-        esperado = "Estado del pretsamo:D"
+        esperado = "Estado del prestamo:D"
         self.assertEquals(estado, esperado)
 
     def test7(self):
 
         estado = funcionalidad.Aprobar_Prestamo(3,70,1.0,3,3)
-        esperado = "Estado del pretsamo:A"
+        esperado = "Estado del prestamo:A"
         self.assertEquals(estado, esperado)
-     
-            
+
 if __name__ == '__main__':
     unittest.main()
