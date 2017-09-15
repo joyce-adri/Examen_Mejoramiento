41c8b6991cf59ea1ff3fe5ce7314b9f636bfe5ca
AnaBelen93 <anita-belen93@hotmail.com>
2017-09-15 10:06:08 -0500

    Update funcionalidad.py

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+#Divorceado=1 , Soltero =2, casado=3  
 
+def Aprobar_Prestamo(aniosTrabajo,capacidadEndeudamiento,aniosViviendaActual,estadoCivil,,dependientes):
+    estado = "D"  #denegado
+    if aniosTrabajo<2:
+       if capacidadEndeudamiento <75:
+          if aniosViviendaActual<1.5:
+             estado="D"
+          elif aniosViviendaActual>=1.5:
+               estado="A"
+       elif capacidadEndeudamiento>=75:
+            estado="M"
+    elif aniosTrabajo>=2:
+         if estadoCivil==1:
+            if dependidentes >0:
+               estado="A"
+            elif dependientes==0:
+                 estado="M"
+         elif estadoCivil==2:
+              estado="D"
+         elif estadoCivil==3: 
+              estado="A"
+       
+    return "Estado del prestamo: %s" % (estado)
