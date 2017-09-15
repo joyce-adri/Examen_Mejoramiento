2e37ff7b0d81e5b4a999c5634b5b4f31e610c900
mvelasco93 <m_velasco93@hotmail.com>
2017-09-15 09:47:20 -0500

    Test4

--- test.py
+++ test.py
@@ -29,6 +29,13 @@ class Test(unittest.TestCase):
         result_val = funcionalidad.aprobarPrestamo(1, "soltero", 20, 3, 1, 22)
         expected_val = "A"
         self.assertEquals(result_val, expected_val)
+
+    def test_prueba_4(self):
+
+        #dependientes, estado, capacidaddeuda, tiempovivienda, tiempotrabajo, edad
+        result_val = funcionalidad.aprobarPrestamo(0, "divorciado", 20, 3, 5, 30)
+        expected_val = "M"
+        self.assertEquals(result_val, expected_val)
             
 if __name__ == '__main__':
     unittest.main()
