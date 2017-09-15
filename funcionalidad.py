3890e224eaa5cae8efcd1b3c8b9cee23ab3e0f71
mavemore <veronica940413@hotmail.com>
2017-09-15 09:12:11 -0500

    primer commit

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+def aprobarPrestamo(aniosTrabajo, estadoCivil, dependientes, capacidadEndeudamiento, aniosVivienda):
+	resultado = "D"
+	if aniosTrabajo >= 2:
+		if estadoCivil == "divorciado":
+			if dependientes > 0:
+				resultado = "A"
+			elif dependientes == 0:
+				resultado = "M"
+		elif estadoCivil == "soltero":
+			resultado = "D"
+		elif estadoCivil == "casado":
+			resultado = "A"
+	else:
+		if capacidadEndeudamiento >= 75:
+			resultado = "M"
+		else:
+			if aniosVivienda >= 1.5:
+				resultado = "A"
+			else:
+				resultado = "D"
+	return resultado
--- test.py
+++ test.py
@@ -6,12 +6,90 @@ import funcionalidad
 
 
 class Test(unittest.TestCase):
-    
+	
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
-    
-    def test_prueba_ID(self):
-        self.assertEquals()
-            
+	
+	def test_prueba_1(self):
+		aniosTrabajo = 2
+		estadoCivil = "soltero"
+		dependientes = 0
+		capacidadEndeudamiento = 0
+		aniosVivienda = 0
+		resultado = funcionalidad.aprobarPrestamo(aniosTrabajo, estadoCivil, dependientes, capacidadEndeudamiento, aniosVivienda)
+		self.assertEquals(resultado, "D")
+
+	def test_prueba_2(self):
+		aniosTrabajo = 2
+		estadoCivil = "casado"
+		dependientes = 0
+		capacidadEndeudamiento = 0
+		aniosVivienda = 0
+		resultado = funcionalidad.aprobarPrestamo(aniosTrabajo, estadoCivil, dependientes, capacidadEndeudamiento, aniosVivienda)
+		self.assertEquals(resultado, "A")
+
+	def test_prueba_3(self):
+		aniosTrabajo = 2
+		estadoCivil = "viudo"
+		dependientes = 0
+		capacidadEndeudamiento = 0
+		aniosVivienda = 0
+		resultado = funcionalidad.aprobarPrestamo(aniosTrabajo, estadoCivil, dependientes, capacidadEndeudamiento, aniosVivienda)
+		self.assertEquals(resultado, "D")
+
+	def test_prueba_4(self):
+		aniosTrabajo = 2
+		estadoCivil = "divorciado"
+		dependientes = 1
+		capacidadEndeudamiento = 0
+		aniosVivienda = 0
+		resultado = funcionalidad.aprobarPrestamo(aniosTrabajo, estadoCivil, dependientes, capacidadEndeudamiento, aniosVivienda)
+		self.assertEquals(resultado, "A")
+
+	def test_prueba_5(self):
+		aniosTrabajo = 2
+		estadoCivil = "divorciado"
+		dependientes = 0
+		capacidadEndeudamiento = 0
+		aniosVivienda = 0
+		resultado = funcionalidad.aprobarPrestamo(aniosTrabajo, estadoCivil, dependientes, capacidadEndeudamiento, aniosVivienda)
+		self.assertEquals(resultado, "M")
+
+	def test_prueba_6(self):
+		aniosTrabajo = 2
+		estadoCivil = "divorciado"
+		dependientes = -1
+		capacidadEndeudamiento = 0
+		aniosVivienda = 0
+		resultado = funcionalidad.aprobarPrestamo(aniosTrabajo, estadoCivil, dependientes, capacidadEndeudamiento, aniosVivienda)
+		self.assertEquals(resultado, "D")
+
+	def test_prueba_7(self):
+		aniosTrabajo = 1
+		estadoCivil = "divorciado"
+		dependientes = 0
+		capacidadEndeudamiento = 76
+		aniosVivienda = 0
+		resultado = funcionalidad.aprobarPrestamo(aniosTrabajo, estadoCivil, dependientes, capacidadEndeudamiento, aniosVivienda)
+		self.assertEquals(resultado, "M")
+
+	def test_prueba_8(self):
+		aniosTrabajo = 1
+		estadoCivil = "divorciado"
+		dependientes = 0
+		capacidadEndeudamiento = 74
+		aniosVivienda = 1.5
+		resultado = funcionalidad.aprobarPrestamo(aniosTrabajo, estadoCivil, dependientes, capacidadEndeudamiento, aniosVivienda)
+		self.assertEquals(resultado, "A")
+
+	def test_prueba_9(self):
+		aniosTrabajo = 1
+		estadoCivil = "divorciado"
+		dependientes = 0
+		capacidadEndeudamiento = 74
+		aniosVivienda = 1
+		resultado = funcionalidad.aprobarPrestamo(aniosTrabajo, estadoCivil, dependientes, capacidadEndeudamiento, aniosVivienda)
+		self.assertEquals(resultado, "D")
+			
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
