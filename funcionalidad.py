04abd92a97cbc5507bac8a23f6388af3e103791a
jebenite <evelynbenitez263@gmail.com>
2017-09-15 09:51:18 -0500

    prueba 1

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+def aprobar_prestamo(anios_t, capac_end, anios_viv, est_civil,dependientes):
+    if anios_t < 2:
+        if capac_end <75:
+            if anios_viv < 1.5:
+                return "denegado"
+            else:
+                return "aprobado"
+        else:
+            return "manual"
+    else:
+        if est_civil == "single":
+            return "denegado"
+        elif est_civil == "married":
+            return "aprobado"
+        else:
+            if dependientes > 0:
+                return "aprobado"
+            else:
+                return "manual"
--- test.py
+++ test.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 import unittest
 import funcionalidad
 
-
 class Test(unittest.TestCase):
-    
-# >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
-# Reemplazar EL ID por el numero correspondiente del caso de prueba
-    
-    def test_prueba_ID(self):
-        self.assertEquals()
-            
+    def test_prueba_1(self):
+        result = funcionalidad.aprobar_prestamo(3,80,1.5,"single",20)
+        self.assertEqual(result,"denegado")
+    '''def test_prueba_2(self):
+        result = funcionalidad.aprobar_prestamo(3,80,1.5,"married",20)
+        self.assertEqual(result,"aprobado")
+    def test_prueba_3(self):
+        result = funcionalidad.aprobar_prestamo(3,80,1.5,"divorced",1)
+        self.assertEqual(result,"aprobado")
+    def test_prueba_4(self):
+        result = funcionalidad.aprobar_prestamo(3,80,1.5,"divorced",0)
+        self.assertEqual(result,"manual")
+    def test_prueba_5(self):
+        result = funcionalidad.aprobar_prestamo(1,80,1.5,"divorced",0)
+        self.assertEqual(result,"manual")
+    def test_prueba_6(self):
+        result = funcionalidad.aprobar_prestamo(1,74,1.5,"divorced",0)
+        self.assertEqual(result,"aprobado")
+    def test_prueba_7(self):
+        result = funcionalidad.aprobar_prestamo(1,74,1.4,"divorced",0)
+        self.assertEqual(result,"denegado")'''
 if __name__ == '__main__':
     unittest.main()
