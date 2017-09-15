00a529e140d2a80eba16337209c4d987fb63ec31
Washington Vélez <wjvelez@espol.edu.ec>
2017-09-15 09:36:31 -0500

    ->TEST3

--- test.py
+++ test.py
@@ -24,13 +24,13 @@ class Test(unittest.TestCase):
         prestamo = funcionalidad.aprobar_prestamo(anios_trab, cap_end, anios_viv, 'soltero', 1)
         self.assertEqual(prestamo, 'A')
 
-    '''
     def test_prueba_3(self):
         anios_trab = 1
         cap_end = 0.80
         prestamo = funcionalidad.aprobar_prestamo(anios_trab, cap_end, 1, 'soltero', 1)
         self.assertEqual(prestamo, 'M')
 
+    '''
     def test_prueba_4(self):
         anios_trab = 3
         estado_civil = 'casado'
