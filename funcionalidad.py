e008419e04c9489bcce0ead41137cbe7bc717fee
odmoreno <oscarpol95@gmail.com>
2017-09-15 09:35:46 -0500

    prueba 1

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+
+def aprobarPrestamo(anios_trabajo, estado_civil, endeudamiento, anios_vivienda, dependientes ):
+    if anios_trabajo >=2 : #1
+        if estado_civil == 'divorced':
+            if dependientes > 0:
+                estado = 'A'
+            elif dependientes == 0 :
+                estado = 'M'
+        elif estado_civil == 'single':
+            estado = 'D'
+        elif estado_civil == 'Married':
+            estado = 'A'
+    elif anios_trabajo <2: #2
+        if endeudamiento >= 75:
+            estado = 'M'
+        elif endeudamiento < 75:
+            if anios_vivienda >= 1.5:
+                estado = 'A'
+            elif anios_vivienda < 1.5:
+                estado = 'D'
+    return  "Calificacion: %s" % (estado)
--- test.py
+++ test.py
@@ -10,8 +10,9 @@ class Test(unittest.TestCase):
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
     
-    def test_prueba_ID(self):
-        self.assertEquals()
+    def test_prueba_1(self):
+        msg = funcionalidad.aprobarPrestamo(3, 'divorced', 0, 0, 1)
+        self.assertEquals(msg, "Calificacion: A")
             
 if __name__ == '__main__':
     unittest.main()
