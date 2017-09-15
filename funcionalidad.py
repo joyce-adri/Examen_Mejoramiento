b37c946f1afa24ae038df90cb4153f216d83f575
Fernando Carvajal <efcu93@gmail.com>
2017-09-15 09:18:31 -0500

    funcionalidad

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+def aprobarPrestamo(aniosTrabajo, capacidadEndeudamiento, aniosVivienda, dependientes, estadoC):
+	solicitud = ""
+    if aniosTrabajo > 2:
+        if estadoC == "soltero":
+            solicitud = "D"
+        elif estadoC == "casado":
+        	solicitud = "A"
+        elif estadoC == "divorciado":
+        	if dependientes == 0:
+        		solicitud = "M"
+    		elif dependientes > 0:
+    			solicitud = "A"
+    else:
+    	if capacidadEndeudamiento >= 75:
+            solicitud = "M"
+        else:
+        	if aniosVivienda >= 1.5:
+        		solicitud = "A"
+    		else:
+    			solicitud = "D"
+   
+    return solicitud
