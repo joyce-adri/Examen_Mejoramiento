5a442709816bb814678d01e267921d48ed7bafd5
LuisBSC15 <luallain@hotmail.com>
2017-09-17 13:04:39 -0500

    Update test.py

--- test.py
+++ test.py
@@ -16,43 +16,43 @@ class Test(unittest.TestCase):
     def test_1(self):
         prestamo= Prestamo(0,"Married",0,0,2,0)
         programa = Programa()
-        msg = programa.AprobarPrestamo(prestamo1)
+        msg = programa.AprobarPrestamo(prestamo)
         self.assertEquals(msg, "A")
 
     def test_2(self):
         prestamo= Prestamo(0,"Single",0,0,2,0)
         programa = Programa()
-        msg = programa.AprobarPrestamo(prestamo1)
+        msg = programa.AprobarPrestamo(prestamo)
         self.assertEquals(msg, "D")
 
     def test_3(self):
         prestamo= Prestamo(1,"Divorced",0,0,2,0)
         programa = Programa()
-        msg = programa.AprobarPrestamo(prestamo1)
+        msg = programa.AprobarPrestamo(prestamo)
         self.assertEquals(msg, "A")
 
     def test_4(self):
         prestamo= Prestamo(0,"Divorced",0,0,2,0)
         programa = Programa()
-        msg = programa.AprobarPrestamo(prestamo1)
+        msg = programa.AprobarPrestamo(prestamo)
         self.assertEquals(msg, "M")
 
     def test_5(self):
         prestamo= Prestamo(0,"",75,0,1,0)
         programa = Programa()
-        msg = programa.AprobarPrestamo(prestamo1)
+        msg = programa.AprobarPrestamo(prestamo)
         self.assertEquals(msg, "M")
 
     def test_6(self):
         prestamo= Prestamo(0,"",74,1,1,0)
         programa = Programa()
-        msg = programa.AprobarPrestamo(prestamo1)
+        msg = programa.AprobarPrestamo(prestamo)
         self.assertEquals(msg, "D")
 
     def test_7(self):
         prestamo= Prestamo(0,"",74,2,1,0)
         programa = Programa()
-        msg = programa.AprobarPrestamo(prestamo1)
+        msg = programa.AprobarPrestamo(prestamo)
         self.assertEquals(msg, "A")
 
 
